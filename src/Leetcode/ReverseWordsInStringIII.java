fb630ff1290379ebf15542cf86873531d69ae4b1
liz <larry.li@dowjoes.com>
2020-08-30 22:38:10 +0800

    翻转字符串III

---
+++ src/Leetcode/ReverseWordsInStringIII.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class ReverseWordsInStringIII {
+    public String reverseWords(String s) {
+        if(s == null || s.isEmpty()) {
+            return "";
+        }
+
+        String[] strs = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(reverseWord(str)).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    private String reverseWord(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        return stringBuilder.reverse().toString();
+    }
+}
