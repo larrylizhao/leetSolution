80c8bb3ec773a62bc652e55b67713309911d8a79
Larry <larrylizhao@gmail.com>
2020-09-10 00:15:39 +0800

    39. 组合总和

--- src/Leetcode/CombinationSum.java
+++ src/Leetcode/CombinationSum.java
@@ -1,5 +1,6 @@
 package Leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,7 +8,35 @@ import java.util.List;
  * #回溯
  */
 public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public static void main(String[] args) {
+        CombinationSum combinationSum = new CombinationSum();
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        List<List<Integer>> res = combinationSum.combinationSum(candidates, target);
+        System.out.println(res);
+    }
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(candidates, target, path, res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, List<Integer> path, List<List<Integer>> res) {
+        if(target < 0) {
+            return;
+        }
+
+        if( target == 0 ) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int candidate : candidates) {
+            path.add(candidate);
+            dfs(candidates, target - candidate, path, res);
+            path.remove(path.size() - 1);
+        }
     }
 }
