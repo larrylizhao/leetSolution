4231fbfa95b83cdee255fc14fb4e7bb23e474da3
Larry <larrylizhao@gmail.com>
2020-10-23 17:07:44 +0800

    234. 回文链表

---
+++ src/Leetcode/oct2020/PalindromeLinkedList.java
@@ -0,0 +1,26 @@
+package Leetcode.oct2020;
+
+/**
+ * 234. 回文链表
+ * #链表 #双指针 #回文
+ */
+public class PalindromeLinkedList {
+    /*
+        请判断一个链表是否为回文链表。
+        示例 1:
+        输入: 1->2
+        输出: false
+        示例 2:
+        输入: 1->2->2->1
+        输出: true
+     */
+    /*
+     思路一:
+        1. find mid node 使用快慢指针找到链表中点
+        2. reverse 逆序后半部分
+        3. check 从头、中点，开始比较是否相同
+     */
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
