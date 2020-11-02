6efcbe43bc724be813186c6ca138be66121c36f2
Larry <larrylizhao@gmail.com>
2020-11-02 17:23:28 +0800

    349. 两个数组的交集

--- src/Leetcode/Nov2020/IntersectionOfTwoArrays.java
+++ src/Leetcode/Nov2020/IntersectionOfTwoArrays.java
@@ -2,7 +2,9 @@ package Leetcode.Nov2020;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 349. 两个数组的交集
@@ -50,6 +52,39 @@ public class IntersectionOfTwoArrays {
         // List<Integer> 转 int[]
         return res.stream().mapToInt(e->e).toArray();
     }
+
+        public int[] intersection_col(int[] nums1, int[] nums2) {
+            Set<Integer> set1 = new HashSet<Integer>();
+            Set<Integer> set2 = new HashSet<Integer>();
+            for (int num : nums1) {
+                set1.add(num);
+            }
+            for (int num : nums2) {
+                set2.add(num);
+            }
+            return getIntersection(set1, set2);
+        }
+
+        public int[] getIntersection(Set<Integer> set1, Set<Integer> set2) {
+            // clean way to always make the param set1 has smaller size
+            if (set1.size() > set2.size()) {
+                return getIntersection(set2, set1);
+            }
+            // HashSet查找的复杂度是O(1)
+            Set<Integer> intersectionSet = new HashSet<>();
+            for (int num : set1) {
+                if (set2.contains(num)) {
+                    intersectionSet.add(num);
+                }
+            }
+            int[] intersection = new int[intersectionSet.size()];
+            int index = 0;
+            for (int num : intersectionSet) {
+                intersection[index++] = num;
+            }
+            return intersection;
+        }
+
 
     public static void main(String[] args) {
         int[] nums1 = {4,9,5};
