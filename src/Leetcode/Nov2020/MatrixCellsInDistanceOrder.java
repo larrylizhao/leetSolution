62deee83e3751e15fe1e22b57a6900e96ef2d07f
Larry <larrylizhao@gmail.com>
2020-11-18 13:30:22 +0800

    1030. 距离顺序排列矩阵单元格

--- src/Leetcode/Nov2020/MatrixCellsInDistanceOrder.java
+++ src/Leetcode/Nov2020/MatrixCellsInDistanceOrder.java
@@ -1,6 +1,8 @@
 package Leetcode.Nov2020;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -27,33 +29,67 @@ public class MatrixCellsInDistanceOrder {
         boolean[][] added = new boolean[R][C];
         List<int[]> res = new ArrayList<>();
         int[] directions = new int[]{0, 1, 0, -1, 0};
+        // 从起始点开始BFS分层遍历距离为1的点, 距离为1递增, 从而根据距离对点进行排序
         Queue<PointRC> points = new LinkedList<>();
         PointRC start = new PointRC(r0, c0);
         points.offer(start);
-        res.add(new int[]{r0, c0});
+        // 记录已加入的点, 去重
         added[r0][c0] = true;
         // 由起点开始依次查找距离为1的点并加入结果数组
+        res.add(new int[]{r0, c0});
         while(!points.isEmpty()) {
+            // 分层BFS
             int len = points.size();
             for (int i = 0; i < len; i++) {
                 PointRC p = points.poll();
                 int r = p.r;
                 int c = p.c;
 
+                // 像四个方向寻找合法的距离为一的点并加入结果列表和节点队列
                 for (int j = 0; j < 4; j++) {
                     int rn = r + directions[j];
                     int cn = c + directions[j + 1];
                     if(rn > -1 && cn > -1 && rn < R && cn < C && !added[rn][cn] && Math.abs(r - rn) + Math.abs(c - cn) == 1) {
-                        res.add(new int[]{rn, rn});
+                        res.add(new int[]{rn, cn});
                         added[rn][cn] = true;
+                        // 将新的点加入队列, 在下一轮进行寻找与其距离为1的点
+                        points.offer(new PointRC(rn, cn));
                     }
                 }
             }
         }
         return res.toArray(new int[][]{});
     }
+
+    // R*C的二维数组存储数组下标, 直接对数组下标根据曼哈顿距离排序
+    public int[][] allCellsDistOrder_sort(int R, int C, int r0, int c0) {
+        // 有R*C个一维数组, 每个一维数组的长度暂未分配
+        // 这个二维数组存储了R行C列矩阵的所有坐标点, 坐标点用一维数组表示
+        int[][] ret = new int[R * C][];
+        // 有R个一维数组, 每个一维数组的长度是C
+        // 数组下标代表坐标点, 该二维数组可以和矩阵每个点的值一一对应
+        int[][] com = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                // 每个一维数组是长度为二的一维数组, 表示R*C矩阵中的所有点
+                ret[i * C + j] = new int[]{i, j};
+//                com[i][j] = i + j;
+            }
+        }
+        // 遍历ret的值(即是12个代表坐标点的一维数组)
+        // lambda中对坐标点距离进行排序
+        Arrays.sort(ret, Comparator.comparingInt(a -> (Math.abs(a[0] - r0) + Math.abs(a[1] - c0))));
+//        Arrays.sort(com, Comparator.comparingInt(i -> (-i[0])));
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        MatrixCellsInDistanceOrder matrixCellsInDistanceOrder = new MatrixCellsInDistanceOrder();
+        matrixCellsInDistanceOrder.allCellsDistOrder_sort(3,4, 0, 1);
+    }
 }
 
+// 存储矩阵中的点
 class PointRC {
     public int r;
     public int c;
