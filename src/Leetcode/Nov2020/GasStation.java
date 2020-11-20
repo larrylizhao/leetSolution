38f993d1959c4e3590d9f90c6390e2bd777419fc
Larry <larrylizhao@gmail.com>
2020-11-20 15:14:05 +0800

    134. 加油站

--- src/Leetcode/Nov2020/GasStation.java
+++ src/Leetcode/Nov2020/GasStation.java
@@ -41,7 +41,7 @@ public class GasStation {
         你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
         因此，无论怎样，你都不可能绕环路行驶一周。
      */
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    public int canCompleteCircuit_brute(int[] gas, int[] cost) {
         int gasSum = Arrays.stream(gas).sum();
         int costSum = Arrays.stream(cost).sum();
         // 总油量小于总路程则一定无法成行
@@ -75,6 +75,53 @@ public class GasStation {
             }
             // 否则继续尝试
             i++;
+        }
+
+        // 尝试完所有站点都不可能
+        return -1;
+    }
+
+    /*
+      考虑进行优化:
+        1. 我们首先检查第 0 个加油站，并试图判断能否环绕一周;
+        2. 如果不能, 就从第一个无法到达的加油站开始继续检查。
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = Arrays.stream(gas).sum();
+        int costSum = Arrays.stream(cost).sum();
+        // 总油量小于总路程则一定无法成行
+        if(costSum > gasSum) {
+            return -1;
+        }
+        int len = gas.length;
+        int i = 0;
+        while(i < len) {
+            // 消耗大于加油的站点无法作为起始站
+            if(gas[i] < cost[i]) {
+                i++;
+                continue;
+            }
+            int cnt = 0;
+            int station = i;
+            int oil = 0;
+            int canNotReach = -1;
+            while(cnt < len) {
+                //可以走到下一站就cnt++, 检查最终是否可以走完
+                oil += gas[station] - cost[station];
+                if(oil >= 0) {
+                    cnt++;
+                    station = ++station % len;
+                } else {
+                    canNotReach = station;
+                    break;
+                }
+            }
+            // 可以走完说明i是可行的
+            if(cnt == len) {
+                return i;
+            }
+            // 否则从无法到达的站点继续尝试
+            i = canNotReach == -1 ? i + 1 : canNotReach + 1;
         }
 
         // 尝试完所有站点都不可能
