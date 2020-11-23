fe4c67a874443e4e462ade606dac6ac7c55bb371
Larry <larrylizhao@gmail.com>
2020-11-23 21:10:07 +0800

    452. 用最少数量的箭引爆气球

---
+++ src/Leetcode/Nov2020/MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,17 @@
+package Leetcode.Nov2020;
+
+/**
+ *  452. 用最少数量的箭引爆气球
+ *  #排序 #贪心
+ */
+public class MinimumNumberOfArrowsToBurstBalloons {
+    /*
+        给你一个数组 points ，其中 points [i] = [xstart,xend], 代表气球直径的起点和终点
+        从x点射出一支箭，如果 xstart < x < xend, 则这支箭可以射爆气球
+        求射爆所有气球所需要最少的箭数
+     */
+    public int findMinArrowShots(int[][] points) {
+        m
+
+    }
+}
