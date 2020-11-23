b385c3cb3bba409a542225302f193e1bd181e35f
Larry <larrylizhao@gmail.com>
2020-11-23 21:15:45 +0800

    148. 排序链表

---
+++ src/Leetcode/Nov2020/SortList.java
@@ -0,0 +1,17 @@
+package Leetcode.Nov2020;
+
+import Leetcode.oct2020.ListNode;
+
+/**
+ *  148. 排序链表
+ *  #排序 #链表
+ */
+public class SortList {
+    /*
+        给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+        你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+     */
+    public ListNode sortList(ListNode head) {
+        return head;
+    }
+}
