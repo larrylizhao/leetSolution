35ba8082be2c69bb8f0669e387f965a6312028f4
Larry <larrylizhao@gmail.com>
2020-11-30 11:28:49 +0800

    976. 三角形的最大周长

---
+++ src/Leetcode/Nov2020/LargestPerimeterTriangle.java
@@ -0,0 +1,23 @@
+package Leetcode.Nov2020;
+
+/**
+ *  976. 三角形的最大周长
+ *  #排序
+ */
+public class LargestPerimeterTriangle {
+    /*
+        数组A是包含代表长度的正整数, 返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+        如果不能形成任何面积不为零的三角形，返回 0。
+        输入：[2,1,2]
+        输出：5
+
+        输入：[3,2,3,4]
+        输出：10
+
+        输入：[1,2,1]
+        输出：0
+     */
+    public int largestPerimeter(int[] A) {
+        return 0;
+    }
+}
