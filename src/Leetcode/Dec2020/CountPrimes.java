4e6f342d87edce748dd1f77917d14ed911d32300
Larry <larrylizhao@gmail.com>
2020-12-03 22:16:14 +0800

    204. 计数质数

---
+++ src/Leetcode/Dec2020/CountPrimes.java
@@ -0,0 +1,19 @@
+package Leetcode.Dec2020;
+
+/**
+ *  204. 计数质数
+ */
+public class CountPrimes {
+    /*
+        统计所有小于非负整数 n 的质数的数量。
+        输入：n = 10
+        输出：4
+        解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+
+        输入：n = 0
+        输出：0
+     */
+    public int countPrimes(int n) {
+
+    }
+}
