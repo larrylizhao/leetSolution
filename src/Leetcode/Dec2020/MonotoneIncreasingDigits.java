0ea35b9857a94626346a2622e2b98d748c0a39e7
Larry <larrylizhao@gmail.com>
2020-12-15 22:36:19 +0800

    738. 单调递增的数字

---
+++ src/Leetcode/Dec2020/MonotoneIncreasingDigits.java
@@ -0,0 +1,35 @@
+package Leetcode.Dec2020;
+
+/**
+ *  738. 单调递增的数字
+ *  #排序
+ */
+public class MonotoneIncreasingDigits {
+    /*
+        找到不大于 n 的最大的数，该数满足从高位到低位的数字非严格递增。
+        输入: N = 1234
+        输出: 1234
+
+        输入: N = 332
+        输出: 299
+
+     */
+    /*
+        从低位向高位遍历，如果高位大于低位，高位-1，低位变为9
+     */
+    public int monotoneIncreasingDigits(int N) {
+        // int 转为 char[]
+        char[] arr = (N + "").toCharArray();
+        //int[] arr = Stream.of(charArr).mapToInt(Integer::intValue).toArray();
+        int len = arr.length;
+        for (int i = len - 1; i > 0; i--) {
+            if(arr[i] < arr[i - 1]) {
+                arr[i] = '9';
+                // char 转为 int , int 转为 char
+                arr[i - 1] = Character.forDigit(arr[i - 1] - '0', 10);
+            }
+        }
+        // char array 转为 String, String 转为 int
+        return Integer.parseInt(new String(arr));
+    }m
+}
