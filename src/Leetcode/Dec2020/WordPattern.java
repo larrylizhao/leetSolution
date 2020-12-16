9356ff37cd22c1fdf31a14b7ce00686445f308f8
Larry <larrylizhao@gmail.com>
2020-12-16 19:14:28 +0800

    290. 单词规律

---
+++ src/Leetcode/Dec2020/WordPattern.java
@@ -0,0 +1,23 @@
+package Leetcode.Dec2020;
+
+/**
+ *  290. 单词规律
+ *  #
+ */
+public class WordPattern {
+    /*
+        给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+        输入: pattern = "abba", str = "dog cat cat dog"
+        输出: true
+
+        输入: pattern = "aaaa", str = "dog cat cat dog"
+        输出: false
+
+        输入:pattern = "abba", str = "dog cat cat fish"
+        输出: false
+     */
+    public boolean wordPattern(String pattern, String s) {
+
+
+    }
+}
