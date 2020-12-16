67cc899418b68900cc88b48b2a2deee699c60e9d
Larry <larrylizhao@gmail.com>
2020-12-16 19:07:57 +0800

    738. 单调递增的数字

--- src/Leetcode/Dec2020/MonotoneIncreasingDigits.java
+++ src/Leetcode/Dec2020/MonotoneIncreasingDigits.java
@@ -1,5 +1,7 @@
 package Leetcode.Dec2020;
 
+import java.util.Arrays;
+
 /**
  *  738. 单调递增的数字
  *  #排序
@@ -18,18 +20,19 @@ public class MonotoneIncreasingDigits {
         从低位向高位遍历，如果高位大于低位，高位-1，低位变为9
      */
     public int monotoneIncreasingDigits(int N) {
-        // int 转为 char[]
-        char[] arr = (N + "").toCharArray();
+        // String 转为 int array
+        int[] arr = (N + "").chars().map(x -> x - '0').toArray();
         //int[] arr = Stream.of(charArr).mapToInt(Integer::intValue).toArray();
         int len = arr.length;
         for (int i = len - 1; i > 0; i--) {
             if(arr[i] < arr[i - 1]) {
                 arr[i] = '9';
                 // char 转为 int , int 转为 char
+                for(int j = )
                 arr[i - 1] = Character.forDigit(arr[i - 1] - '0' - 1, 10);
             }
         }
-        // char array 转为 String, String 转为 int
-        return Integer.parseInt(new String(arr));
+        // int array 转为 String, String 转为 int
+        return Integer.parseInt(Arrays.toString(arr));
     }
 }
