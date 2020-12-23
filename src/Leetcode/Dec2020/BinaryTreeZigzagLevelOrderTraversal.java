5e85bab08662c904a01a2b3128ac67a33870e1b8
Larry <larrylizhao@gmail.com>
2020-12-23 11:13:15 +0800

    103. 二叉树的锯齿形层序遍历

---
+++ src/Leetcode/Dec2020/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,29 @@
+package Leetcode.Dec2020;
+
+import java.util.List;
+
+import Leetcode.TreeNode;
+
+/**
+ *  103. 二叉树的锯齿形层序遍历
+ *  #树
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+    /*
+        给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+        给定二叉树 [3,9,20,null,null,15,7],
+            3
+           / \
+          9  20
+            /  \
+           15   7
+         [
+           [3],
+           [20,9],
+           [15,7]
+         ]
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+}
