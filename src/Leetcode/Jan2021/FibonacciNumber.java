c0c1fa41be4669ff6f5d49e46bbc94ed22896c09
Larry <larrylizhao@gmail.com>
2021-01-04 17:42:15 +0800

    509. 斐波那契数

--- src/Leetcode/Jan2021/FibonacciNumber.java
+++ src/Leetcode/Jan2021/FibonacciNumber.java
@@ -17,7 +17,20 @@ public class FibonacciNumber {
             return 1;
         }
 
-
         return fib(n - 1) + fib(n - 2);
+    }
+
+    /*
+        动态规划
+     */
+    public int fib_dp(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
