547d720c920820551c9f0d3fcb65192760c5bdda
Larry <larrylizhao@gmail.com>
2021-03-03 23:01:17 +0800

    338. 比特位计数

---
+++ src/Leetcode/Mar2021/CountingBits.java
@@ -0,0 +1,21 @@
+package Leetcode.Mar2021;
+
+/**
+ *  338. 比特位计数
+ *  #位运算 #动态规划
+ */
+public class CountingBits {
+    /*
+        输入: 数字 num
+        输出: 0 - num 所表示的二进制数中1的个数的数组
+
+        输入: 2
+        输出: [0,1,1]
+
+        输入: 5
+        输出: [0,1,1,2,1,2]
+     */
+    public int[] countBits(int num) {
+
+    }
+}
