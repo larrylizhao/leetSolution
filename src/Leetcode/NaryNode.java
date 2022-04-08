bea8d6b64650a87caa78ff3783410734185291de
larrylizhao <larrylizhao@gmail.com>
2022-04-08 17:45:57 +0800

    node for Nary tree

---
+++ src/Leetcode/NaryNode.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+import java.util.List;
+
+public class NaryNode {
+        public int val;
+        public List<NaryNode> children;
+
+        public NaryNode() {}
+
+        public NaryNode(int _val) {
+            val = _val;
+        }
+
+        public NaryNode(int _val, List<NaryNode> _children) {
+            val = _val;
+            children = _children;
+        }
+}
