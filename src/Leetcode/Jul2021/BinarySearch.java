4a6ac3b0f782527fb0639d6f9f8d5327b7405571
larrylizhao <larrylizhao@gmail.com>
2022-07-25 14:45:40 +0800

    二分查找

--- src/Leetcode/Jul2021/BinarySearch.java
+++ src/Leetcode/Jul2021/BinarySearch.java
@@ -16,17 +16,22 @@ public class BinarySearch {
         int left = 0;
         int right = len - 1;
 
-        while(left <= right) {
+        while(left <= right) { // 搜索区间, 即mid在区间内可能的取值范围: [left, right]
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] > target) { //target应该落在左区间
+            } else if(nums[mid] > target) { // target应该落在左区间, 更新区间为: [left, mid-1]
                 right = mid - 1;
-            } else if(nums[mid] < target) { //target应该落在右区间
+            } else if(nums[mid] < target) { // target应该落在右区间, 更新区间为: [mid+1, right]
                 left = mid + 1;
             }
         }
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
+        bs.search(new int[] {1, 2, 3, 4, 5}, 5);
     }
 }
