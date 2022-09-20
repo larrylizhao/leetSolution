c466e93690dd6136663d6df7a9de6ca75af88efe
larrylizhao <larrylizhao@gmail.com>
2022-09-20 14:12:12 +0800

    二分

--- src/Leetcode/Jul2022/PeakIndexInMountainArray.java
+++ src/Leetcode/Jul2022/PeakIndexInMountainArray.java
@@ -21,4 +21,36 @@ public class PeakIndexInMountainArray {
         }
         return left;
     }
+
+    public static int peakIndexInMountainArray_simp(int[] arr) {
+        int left = 1;
+        int right = arr.length - 2;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid - 1] > arr[mid]) { // mid与mid左侧的元素比较, 如果mid是答案，right=mid不会触发，所以答案要减1
+                right = mid;    //通过确定 right=mid会不会触发来决定答案是 right 还是 right - 1
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right - 1;
+    }
+
+    public static int peakIndexInMountainArray_simp2(int[] arr) {
+        int left = 1;
+        int right = arr.length - 2;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] > arr[mid + 1]) { // mid与mid右侧的元素比较，如果mid是答案，right=mid会触发，所以返回right
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        PeakIndexInMountainArray.peakIndexInMountainArray_simp(new int[] {0, 10, 11, 5, 1});
+    }
 }
